715a3e717b7490ffcc6949f2ab5d73a20edf949b
Graham <git@grahamirvine.com>
2022-03-15 15:39:33 -0600

    Completed exercise 01 and 02 with all tests passed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
-
-};
+const repeatString = function(string, num) {
+  if (num === 0) {
+    return '';
+  } else if (num < 0) {
+    return "ERROR";
+  }
+  let newPhrase = string;
+  for (let i = 1; i < num; i++) {
+   newPhrase += string;
+  }
+  return newPhrase;
+}
 
 // Do not edit below this line
 module.exports = repeatString;
