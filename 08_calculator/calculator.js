c6394d449c10571098ccd53ca52874f728344413
Graham <git@grahamirvine.com>
2022-03-16 16:41:35 -0600

    completed exercise 08, took a little bit to get sum one right because it used arrays. But understood what I was doing

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,46 @@
-const add = function() {
+const add = function(num1, num2) {
+  return num1 + num2;
 	
 };
 
-const subtract = function() {
+const subtract = function(num1, num2) {
+  return num1 - num2;
 	
 };
 
-const sum = function() {
+const sum = function(numArray) {
+  let totalSum = 0;
+  let arrayNum = 0;
+  for (let i = 0; i < numArray.length; i++) {
+    arrayNum = numArray.slice(i, i + 1);
+    arrayNum = parseInt(arrayNum);
+    totalSum += arrayNum;
+  }
+  return totalSum;
+};
+
+const multiply = function(numArray) {
+  let finalMulti = 1;
+  let arrayNum = 0;
+  for (let i = 0; i < numArray.length; i++) {
+    arrayNum = numArray.slice(i, i +1);
+    arrayNum = parseInt(arrayNum);
+    finalMulti *= arrayNum;
+  }
+  return finalMulti;
+};
+
+const power = function(num1, num2) {
+  return num1 ** num2;
 	
 };
 
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(num) {
+  let factNum = 1;
+  for (let i = 1; i <= num; i++) {
+    factNum *= i;
+  }
+  return factNum;	
 };
 
 // Do not edit below this line
@@ -30,4 +51,12 @@ module.exports = {
   multiply,
   power,
   factorial
+};
+
+
+function print_my_arguments(/**/){
+  var args = arguments;
+  for(var i=0; i<args.length; i++){
+      console.log(args[i]);
+  }
 };
