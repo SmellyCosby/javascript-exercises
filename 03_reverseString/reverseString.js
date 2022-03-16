1714c7e8906bd03b6f72db47ea36312dd8d146c4
Graham <git@grahamirvine.com>
2022-03-16 10:51:26 -0600

    completed javascript exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function(string) {
+  let myArray = string.split('');
+  let lastLetter;
+  let reverseWord = '';
 
+  for (let i = myArray.length - 1; i >= 0; i--) {
+    lastLetter = myArray.slice(i, i + 1);
+    reverseWord += lastLetter;
+  }
+  return reverseWord;
 };
 
 // Do not edit below this line
