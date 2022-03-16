f4e9121d0119749a60b7cd0c31e467b3b4dcccf7
Graham <git@grahamirvine.com>
2022-03-16 14:53:27 -0600

    completed exercise 04, found an example on stack overflow that worked, Did my best to understand what it did before using.'
    a
    esc
    '
    ''

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,12 +1,21 @@
-const removeFromArray = function(arr, removedValue) {
-  
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] === removedValue) {
-      arr.splice(i, 1);
-    }
+//const removeFromArray = function(arr, removedValue) {
+//  for (let i = 0; i < arr.length; i++) {
+//    if (arr[i] === removedValue) {
+//      arr.splice(i, 1);
+//    }
+//  }
+//  return arr;
+// };
+
+const removeFromArray = function(arr, ...args) {
+  args.forEach((arg) => {
+    let index = arr.indexOf(arg);
+      if (index > -1) {
+        arr.splice(index, 1);
+      }
+    });
+    return arr;
   }
-  return arr;
-  };
 
 // Do not edit below this line
 module.exports = removeFromArray;
