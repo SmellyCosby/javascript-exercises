bc9a3790a5c2c7add3e693b204c6bbb70c3e2d1b
Graham <git@grahamirvine.com>
2022-03-16 15:41:45 -0600

    completed exercise 07, understood easily.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,14 @@
-const ftoc = function() {
+const ftoc = function(f) {
+  let c = (f - 32) * (5/9);
+  let roundedC = Math.round(c * 10)/10;
+  return roundedC;
 
 };
 
-const ctof = function() {
+const ctof = function(c) {
+  let f = c * (9/5) + 32;
+  let roundedF = Math.round(f * 10)/10;
+  return roundedF;
 
 };
 
