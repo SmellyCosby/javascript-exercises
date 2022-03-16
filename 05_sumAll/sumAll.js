9dcf3982c26a086abb020ffd672546c2b424a058
Graham <git@grahamirvine.com>
2022-03-16 15:20:54 -0600

    completed exercise 05, and felt like I understood it all

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,23 @@
-const sumAll = function() {
-
-};
+const sumAll = function(x, y) {
+  let totalSum = 0;
+  
+  if (typeof(x) != 'number' || typeof(y) != 'number') {
+    return 'ERROR';
+  } else if (x < 0 || y < 0) {
+    return 'ERROR';
+  } else {  
+    if (x < y) {
+      for (i = x; i <= y; i++) {
+        totalSum += i;
+      }
+    } else {
+      for (i = y; i <= x; i++) {
+        totalSum += i;
+      }
+    }
+      return totalSum;
+  }
+}
 
 // Do not edit below this line
 module.exports = sumAll;
