08fa55788dd4e098e1a42d4992763a283b7736af
Graham <git@grahamirvine.com>
2022-03-16 11:26:13 -0600

    working on exerise 04, passed first test but still working on second

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,12 @@
-const removeFromArray = function() {
-
-};
+const removeFromArray = function(arr, removedValue) {
+  
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === removedValue) {
+      arr.splice(i, 1);
+    }
+  }
+  return arr;
+  };
 
 // Do not edit below this line
 module.exports = removeFromArray;
