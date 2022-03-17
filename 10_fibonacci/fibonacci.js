4ee23c88eaa4659516f4e9227edb81e4690c4dd0
Graham <git@grahamirvine.com>
2022-03-17 11:28:07 -0600

    finished exercise 10. Had trouble with it pulling out arrays. Decided was too complicated and started over by setting first two values as they are always same. Works well.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+  let numArray = [1];
+  let lastNum = 1;
+  let secondLastNum = 0;
+
+  if (num < 0) {
+    return 'OOPS';
+  } else {
+    num = parseInt(num);
+
+    for (let i = 1; i < num; i ++) {
+      nextNum = lastNum + secondLastNum;
+      lastNumHold = lastNum;
+      lastNum = nextNum;
+      secondLastNum = lastNumHold;
+      numArray.push(nextNum);
+  }
+  return parseInt(numArray[numArray.length - 1]);
+}
 
 };
 
