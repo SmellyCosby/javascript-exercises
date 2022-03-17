ad8a308e8df7f7004c9d4a4963878038e20e8956
Graham <git@grahamirvine.com>
2022-03-17 14:53:19 -0600

    completed exercise 12, took some time to figure out how to access objects and get current year

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,28 @@
-const findTheOldest = function() {
+const findTheOldest = function(obj) {
+  let birthYear;
+  let deathYear;
+  let age = [];
+  let yearToday = new Date();
 
-};
+
+  for (i = 0; i < obj.length; i++) {
+    if (obj[i].hasOwnProperty('yearOfDeath')) {
+    birthYear = obj[i]['yearOfBirth'];
+    deathYear = obj[i]['yearOfDeath'];
+    personAge = deathYear - birthYear;
+    age.push(personAge);
+    } else {
+      birthYear = obj[i]['yearOfBirth'];
+      deathYear = yearToday.getFullYear();
+      personAge = deathYear - birthYear;
+      age.push(personAge);
+    }
+  }
+  let oldestPerson = age.indexOf(Math.max(...age));
+  return obj[oldestPerson];
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
+
+
