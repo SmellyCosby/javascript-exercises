e98d766a582fe3b8f14318ef9ec2166bae827d3d
Graham <git@grahamirvine.com>
2022-03-17 16:20:14 -0600

    working through exercise 13 stuck on large shift numbers, but wanted to commit before deleting code and trying new technique

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,4 +1,38 @@
-const caesar = function() {
+const caesar = function(phrase, shiftNum) {
+  let encryptedPhrase = '';
+  let phraseLetter;
+  let alphaLow = 'abcdefghijklmnopqrstuvwyxz';
+  let alphaUp = 'ABCDEFGHIJKLMNOPQRSTUVWYXZ';
+  const regex = /[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g;
+
+  for  (i = 0; i < phrase.length; i++) {
+    phraseLetter = phrase.substring(i, i + 1);
+    if (!!phraseLetter.match(regex) == true || !!phraseLetter.match(' ') == true) {
+      encryptedPhrase += phraseLetter;
+    } else if (phraseLetter.toUpperCase() === phraseLetter) {
+      shiftedLetterPosition = alphaUp.indexOf(phraseLetter) + shiftNum;
+      if (shiftedLetterPosition > 25) {
+        shiftedLetterPosition = shiftedLetterPosition - 26;
+      }
+      if (shiftedLetterPosition < 0) {
+        shiftedLetterPosition = shiftedLetterPosition + 26;
+       }           
+      shiftedLetter = alphaUp.substring(shiftedLetterPosition, shiftedLetterPosition + 1);
+      encryptedPhrase += shiftedLetter;
+    } else {
+        shiftedLetterPosition = alphaLow.indexOf(phraseLetter) + shiftNum;
+        if (shiftedLetterPosition > 25) {
+          shiftedLetterPosition = shiftedLetterPosition - 26;
+        }
+        if (shiftedLetterPosition < 0) {
+          shiftedLetterPosition = shiftedLetterPosition + 26;
+         } 
+        shiftedLetter = alphaLow.substring(shiftedLetterPosition, shiftedLetterPosition + 1);
+        encryptedPhrase += shiftedLetter;
+    }
+  }
+  return encryptedPhrase;
+
 
 };
 
