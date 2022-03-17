39965cd34c111298eaf2c3dea7f80fc25167d02d
Graham <git@grahamirvine.com>
2022-03-17 10:26:43 -0600

    finished exercise 09, was pretty easy, I modified code from exercise 03

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,25 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let reverseArray = string.split('');
+    let lastLetter;
+    let reverseWord = '';
 
-};
+    for (i =reverseArray.length - 1; i >= 0; i--) {
+        lastLetter = reverseArray.slice(i, i + 1);
+        reverseWord += lastLetter;
+    }
+    let reverseWordNoPunctuation = reverseWord.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"", '');
+    let reverseWordNoSpace = reverseWordNoPunctuation.replace(/\s/g, '');
+    let stringNoPunc = string.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"", '');
+    let stringNoSpace = stringNoPunc.replace(/\s/g, '');
+    stringNoSpace = stringNoSpace.toLowerCase();
+    reverseWordNoSpace = reverseWordNoSpace.toLowerCase();
+   if (reverseWordNoSpace === stringNoSpace) {
+       return true;
+   } else {
+       return false;
+   }
+}
+
 
 // Do not edit below this line
 module.exports = palindromes;
